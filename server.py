216584110650de98aa0ff232b4e72c418b8fb1d8
Ael <leonardi.armand@gmail.com>
2023-02-05 17:01:11 +0100

    add parser and subprocess command

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from main_rental import Main
 from ast import literal_eval
+import subprocess
+from argparse import ArgumentParser
+from Core import Base64Engine
+import json
 
 app = Flask("qrt_rental_compagny_server")
-main = Main()
+config = {}
 
 def get_inputs() -> str:
 
@@ -27,16 +31,45 @@ def optimize():
 
     if get_inputs_ok is True:
 
-        main.list_of_contract = raw_list_of_contracts
+        raw_list_of_contracts_b64 = Base64Engine.encode(raw_list_of_contracts)
 
-        result = main.run()
+        target_script = config.get("target_script")
+        target_script_config = config.get("target_script_config")
+        verbose = "--verbose" if config.get("verbose") is True else ""
+        debug = "--debug" if config.get("debug") is True else ""
+        formated = "--formated" if config.get("formated") is True else ""
+
+        subprocess.run(f"python {target_script} --config {target_script_config} {verbose} {debug} {formated} --contracts {raw_list_of_contracts_b64}")
+
 
     else:
         result = raw_list_of_contracts
 
     return jsonify(result)
 
+subprocess.run("python main_rental.py --config")
 
 if __name__ == "__main__":
+
+    parser = ArgumentParser()
+    parser.add_argument("--config", help="Configuration path use by Main.py" ,type=str, default="")
+    parser.add_argument("--verbose", help="Display details if true. False otherwise.", action='store_true')
+    parser.add_argument("--debug", help="Display more details if true. False otherwise.", action='store_true')
+    parser.add_argument("--formated", help="Format displayed messges if true. False otherwise.", action='store_true')
+    parser.add_argument("--host", help="Host where run the applicaton", type=str, default="127.0.0.1")
+    parser.add_argument("--port", help="Listening port", type=int, default=8080)
+
+    args = parser.parse_args()
+
+    config_path = args.config.strip()
+    host = args.host.strip()
+    port = args.port
+    verbose = args.verbose
+    debug = args.debug
+    formated = args.formated
+
+    with open(config_path, 'r', encoding="utf8") as f:
+        config = json.load(f)
+        f.close()
 
     app.run(debug=True, host="127.0.0.1", port=8080)
