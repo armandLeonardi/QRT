59849a9c9cec01347a755bd4512b88a255f6f320
Ael <leonardi.armand@gmail.com>
2023-02-05 10:00:16 +0100

    creation

---
+++ Contract.py
@@ -0,0 +1,10 @@
+
+class Contract:
+
+    def __init__(self, name: str, start_hour: int, duration: int, price: int):
+        self.name = name
+        self.start_hour = start_hour
+        self.duration = duration
+        self.price = price
+
+    
---
+++ Solver.py
@@ -0,0 +1,7 @@
+
+class Solver:
+
+    def __init__(self):
+        self.__version__ = "1.0.0"
+
+    def 
