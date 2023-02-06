e1ca24cf531013bdb5b0c80625b59f49a68ea869
Ael <leonardi.armand@gmail.com>
2023-02-06 18:21:44 +0100

    add documentation

--- main_rental.py
+++ main_rental.py
@@ -1,5 +1,12 @@
+"""
+This script is a part of small rental compagny simulation ask by QRT for application process.
+This script allow to launch the Main.py script from a console.
+
+Ael - 06FEB23
+"""
+
 from Main import Main
-from argparse import ArgumentParser
+from argparse import ArgumentParser # parse the inputs arguements
 from Core import Base64Engine
 from ast import literal_eval
 
@@ -14,21 +21,28 @@ if __name__ == '__main__':
     
     args = parser.parse_args()
 
+    # set arguments to local variables
     config = args.config.strip()
     contracts = literal_eval(Base64Engine.decode(args.contracts.strip()))
     verbose = args.verbose
     debug = args.debug
     formated = args.formated
 
+    # instance the Main object
     current_main = Main(path_config=config, verbose=verbose, debug=debug, formated=formated)
 
+    # load the configuration
     current_main.load_config()
 
+    # open the log file
     current_main.open_log_file()
 
+    # launch the main method (optimize the contracts path)
     result = current_main.run(list_of_contracts=contracts)
 
+    # close current log file
     current_main.close_log()
 
+    # return on standard output the optimized result
     print(result)
     exit(0)
