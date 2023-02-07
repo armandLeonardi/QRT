66e38dea8bb9804bdb2a7e0aa49f635c4ae46166
Ael <leonardi.armand@gmail.com>
2023-02-07 18:39:18 +0100

    remove concat, get start contract and set windows attribute

--- Solver.py
+++ Solver.py
@@ -6,18 +6,21 @@ This script containt classes:
 You can check on the '__main__' part to see how to use thoose classes
 Ael - 06FEB23
 """
+
 import pandas as pd
 from Core import Core
 import warnings
 warnings.filterwarnings('ignore')
 
+
 class Solver(Core):
 
-    def __init__(self, list_of_contracts: list, verbose: bool = False, debug: bool = False, formated: bool = False):
+    def __init__(self, list_of_contracts: list, windows: int, verbose: bool = False, debug: bool = False, formated: bool = False):
         """_summary_
 
         Args:
             list_of_contracts (list): list of contracts to optimize
+            windows (int): the considered time windows of contracts
             verbose (bool, optional): true if you want to display details. False otherwise. Defaults to False.
             debug (bool, optional): true if you want to display more details. Fale otherwise. Defaults to False.
             formated (bool, optional): true if you want a formated display message. Defaults to False.
@@ -30,24 +33,7 @@ class Solver(Core):
         self.result = {"income": 0, "path": []}
         self.start = -1
         self.duration = -1
-
-    def _concat(self, A: pd.DataFrame, B: pd.DataFrame) -> pd.DataFrame:
-        """Concatenate the two dataframe in inputs.
-        Row concatenation.
-
-        Args:
-            A (pd.DataFrame): head dataframe
-            B (pd.DataFrame): foot dataframe
-
-        Returns:
-            pd.DataFrame: concatenate pandas Dataframe
-        """
-
-        if B is not None:
-            C = pd.concat([A, B])
-        else:
-            C = A
-        return C
+        self.windows = windows
 
     def maximize_price(self) -> list:
         """Maximize algorithm. Main part of the project.
@@ -61,80 +47,64 @@ class Solver(Core):
         while self.is_ended is False:
 
             # get next contract in order to maximize the price
-            current_contract = self.get_next_contract()
+            self.get_next_contract()
 
             # remove selected contract
-            self.drop_contracts(current_contract)
+            self.drop_contracts()
 
     def update_result(self, contract: pd.DataFrame) -> None:
 
         self.result["income"] += contract["price"].values[0]
         self.result["path"].append(contract["name"].values[0])
 
-    def get_start_contract(self) -> pd.DataFrame:
-        """Return the contract with the smallest start date.
-
-        Returns:
-            pd.DataFrame: a contract as Dataframe type.
+    def drop_contracts(self) -> None:
+        """Remove contracts which are too old to be accepted.
+        And then became useless.
         """
 
-        sub_list = self.list_of_contracts[self.list_of_contracts['start'] == self.list_of_contracts['start'].min()]
-
-        sub_list['ratio'] = sub_list['price'] / (sub_list['duration'] - sub_list['start'])
-        first_contract = sub_list[sub_list['ratio'] == sub_list['ratio'].max()]
-
-        return first_contract[['name', 'start', 'duration', 'price']]
-
-    def drop_contracts(self):
-        """Remove the contract in input of the list_of_contracts attribute.
-
-        Args:
-            current_contract (pd.DataFrame): contract to remove
-        """
-
-        #current_time = current_contract['start'] + current_contract['duration']
         current_time = self.start + self.duration
         ended_contracts = self.list_of_contracts[self.list_of_contracts['start'] < current_time.values[0]]
         self.list_of_contracts = self.list_of_contracts.drop(ended_contracts.index)
 
 
-    def get_next_contract(self) -> pd.DataFrame:
+    def get_next_contract(self):
         """Select the next contract in the list_of_contract in order to maximize the income.
+            Add it name and price to result attribute.
 
         Method:
-        - set the current time (by extracting the duraction of current contract)
-        - select the list of possible contracts by only selecting contract with a start date greater of egal of the current time
+        - select the list of possible contracts on given windows.
         - select the contract on this list with return the higher price
           Continue this selection while the next contracts list was emty
-
-        Args:
-            current_contract (pd.DataFrame): juste finished contract
-
-        Returns:
-            pd.DataFrame: next selected contract
         """
 
         next_contract = None
 
-        #current_time = current_contract["start"] + current_contract["duration"]
-        next_possible_contracts = self.list_of_contracts[self.list_of_contracts['start'] <= self.list_of_contracts['start'].min() + 200]
+        # made a selection of possible contract with a given windows
+        next_possible_contracts = self.list_of_contracts[self.list_of_contracts['start'] <= self.list_of_contracts['start'].min() + self.windows]
 
         self.debug(f"List of next possibles contracts {next_possible_contracts.to_json()}")
 
         if next_possible_contracts.empty is False:
 
+            # compute the profitable ratio price / duration ($/h for example)
             next_possible_contracts['ratio'] = next_possible_contracts['price'] / next_possible_contracts['duration']
+
+            # select next contract with higher ratio
             next_contract = next_possible_contracts[next_possible_contracts['ratio'] == next_possible_contracts['ratio'].max()]
 
+            # if there more than 1 contract with the equal higher ratio then select the closest
             if next_contract.shape[0] > 1:
                 next_contract = next_contract[next_contract['start'] == next_contract['start'].min()]
 
+            # extract contract name and inform user
             contract_name = next_contract["name"].values[0]
             self.info(f"current contract {contract_name}, remaining contracts {len(self.list_of_contracts)}")
 
+            # update start and duration (in order to compute next current time)
             self.start = next_contract["start"]
             self.duration = next_contract["duration"]
 
+            # update returned result
             self.update_result(next_contract)
 
         else:
